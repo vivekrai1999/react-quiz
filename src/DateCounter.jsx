4c3df123a6f8605f47322a459795621a44d1677e
vivekrai1999 <vivek.rai.2807@gmail.com>
2024-02-03 10:50:20 +0530

    used dispatch with no payload is reset

--- src/DateCounter.jsx
+++ src/DateCounter.jsx
@@ -1,42 +1,50 @@
 import { useReducer, useState } from "react";
 
 function reducer(state, action){
-  if(action.type === "inc") return state + 1;
-  if(action.type === "dec") return state - 1;
-  if(action.type === "setCount") return action.payload;
+  console.log(state, action);
+  switch(action.type){
+    case "inc":
+      return {...state, count: state.count + state.step}
+    case "dec":
+      return {...state, count: state.count - state.step}
+    case "setCount":
+      return {...state, count: action.payload}
+    case "setStep":
+      return {...state, step: action.payload}
+    case "reset":
+      return {count: 0, step: 1}
+    default:
+      throw new Error('case not matched')
+  }
 }
 
 function DateCounter() {
-  const [step, setStep] = useState(1);
-  const [count, dispatch] = useReducer(reducer, 0)
-
+  //const [step, setStep] = useState(1);
+  const initialState = {count: 0, step: 1}
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const {count, step} = state;
   // This mutates the date object.
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    // setCount((count) => count - 1);
     dispatch({type: "dec"})
   };
 
   const inc = function () {
-    // setCount((count) => count + 1);
-    //setCount((count) => count + step);
     dispatch({type: "inc"})
   };
 
   const defineCount = function (e) {
-    //setCount(Number(e.target.value));
     dispatch({type: "setCount", payload: Number(e.target.value)})
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    dispatch({type: "setStep", payload: Number(e.target.value)})
   };
 
   const reset = function () {
-    setCount(0);
-    setStep(1);
+    dispatch({type: "reset"})
   };
 
   return (
