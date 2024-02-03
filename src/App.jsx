bf2f880c85369896af5727be2fe501b37736def4
vivekrai1999 <vivek.rai.2807@gmail.com>
2024-02-03 16:43:16 +0530

    made states using useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
-import DateCounter from "./DateCounter"
+import { useEffect, useReducer } from "react"
+import Header from "./Header"
+import MainApp from "./MainApp"
+
+const initialState = {
+  questions: [],
+  // loading, error, ready, active, finished
+  status: "loading"
+}
+
+function reducer(state, action){
+  switch(action.type){
+    case "dataReceived":
+      return {...state, questions: action.payload, status: "ready"}
+    case "dataFailed":
+      return {...state, status: "error"}
+    default:
+      throw new Error("Error")
+  }
+}
 
 function App() {
-  return <div>
-    <DateCounter/>
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(()=>{
+    fetch('http://localhost:8000/questions')
+    .then(res=>res.json())
+    .then(data=>dispatch({type:"dataReceived", payload: data}))
+    .catch((err)=>dispatch({type: "dataFailed"}))
+  },[])
+
+  return <div className="app">
+    <Header />
+    <MainApp>
+      <p>1/15</p>
+    </MainApp>
   </div>
 }
 
---
+++ src/MainApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MainApp({children}) {
+  return (
+    <main className='main'>
+        {children}
+    </main>
+  )
+}
+
+export default MainApp
